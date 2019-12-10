4335f179799e0033f319b82fb77194d2e16d02f7
lk321 <orozco6453@gmail.com>
2019-12-10 08:45:32 -0800

    Updates

---
+++ precache-manifest.1e2a3c81855dc365dd6e3c6025e4de18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3844a5b02ef674a65f7813d8e0acd7f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c12cf2404e9687b3fabd",
+    "url": "/monsters-rolodex/static/css/main.d8c01038.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "c12cf2404e9687b3fabd",
+    "url": "/monsters-rolodex/static/js/main.78d7f3e1.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
